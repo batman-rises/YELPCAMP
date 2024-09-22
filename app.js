65af7765dab01cb2b439ce4e196bec2e169a0115
Binayak Panda <pbinayak604@gmail.com>
2024-09-22 10:09:47 +0530

    22/09

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const methodOverride = require('method-override')
 //requiring and connecting MONGOOSE
 const mongoose = require('mongoose');
 const dbUrl = 'mongodb://localhost:27017/yelp-camp';//yelp-camp is name of DB
-
 mongoose.connect(dbUrl)
     .then(() => {
         console.log("Connected to MongoDB successfully!");
@@ -19,23 +18,26 @@ mongoose.connect(dbUrl)
     useNewUrlParser: true,      these all shits are now there by default
     useUnifiedTopology: true,
 })*/
-
-
 const Campground = require('./models/campgrounds')
 
-//requiring EJS-MATE
+//requiring EJS-MATE ~ for using that layouts thing
 const ejsMate = require('ejs-mate');
 
+const { campgroundSchema } = require('./schemas.js')
+
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
+//const Joi = require('joi');
 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));//to execute EJS smoothly we need these two lines
 
-app.use(express.urlencoded({ extended: true }));//parsing through url/ie tempating
+app.use(express.urlencoded({ extended: true }));//parsing through url/ie templating
 
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));//are wo put,patch etc use karne
 
-app.engine('ejs', ejsMate);
+app.engine('ejs', ejsMate);//imp
 
 
 app.get('/', (req, res) => {
@@ -48,15 +50,26 @@ app.get('/', (req, res) => {
     res.send(camp);
 })*/
 
+const validateCampground = (req, res, next) => {
+
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        const mssg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(mssg, 400)
+    } else {
+        next()
+    }
+}
+
+
 
 //Campground is the name of the model
-
 //campground INDEX:-
-app.get('/campgrounds', async (req, res) => {//-> '/campgrounds' ye waala jo hai wo url me hota hai
+app.get('/campgrounds', catchAsync(async (req, res) => {//-> '/campgrounds' ye waala jo hai wo url me hota hai
     const campgrounds = await Campground.find({});
     //res.render('campgrounds/index') here i just grabbed it
     res.render('campgrounds/index', { campgrounds })//grabbed + rendered
-})
+}))
 //Campground New & Create   ~~~order does matter
 
 /**The GET /campgrounds/new route shows a form to create a new campground.
@@ -65,32 +78,49 @@ saves it to the database, and then redirects the user to the details page of the
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+    //if (!req.body.campground) throw new ExpressError('invalid campground data', 400)
+
+    //this is not a mongoose schema but a schema meant for validation in the server side
+
     const campground = new Campground(req.body.campground)//kyunki agar html form ko dhyaan se dekhega to assume karega ki apan saara data campground[] me rakha hai
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+
+}))
 //Campground SHOW~details pg for each camp
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-})
+}))
 //Campground Edit & Update
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
-})
-app.put('/campgrounds/:id', async (req, res) => {
+}))
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });//spread operator
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 //Campground Delete
 app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
 })
+
+app.all(/(.*)/, (req, res, next) => {
+    next(new ExpressError('Pg not found', 404))
+})
+app.use((err, req, res, next) => {
+    //const { statusCode = 500, message = 'Something went wrong' } = err;//also here default mssg and statutscode provided
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'oh no,something went wrong'
+    res.status(statusCode).render('error', { err });
+})
+
+
 
 
 app.listen(3000, () => {
--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,5 @@
+// THIS FILE'S ONLY PURPOSE IS TO FEED THE DB WITH CAMPGROUNDs TITLE AND LOCATION and other stuffs
+
 const mongoose = require('mongoose')
 const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl)
@@ -12,19 +14,21 @@ mongoose.connect(dbUrl)
 // so gotta put double dots ..
 const Campground = require('../models/campgrounds')
 const cities = require('./cities')
+
 const { places, descriptors } = require('./seedHelpers')
 
 //picking a random element from the array
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-
-
 const seedDB = async () => {
     await Campground.deleteMany({});
+
     //const c = new Campground({ title: 'purple field' });
+
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
+
         const camp = new Campground({
             location: `${cities[random1000].city},${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
@@ -32,11 +36,10 @@ const seedDB = async () => {
             description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, nam? Nemo placeat, officia quia sequi non vero, impedit temporibus consequuntur numquam quasi sapiente distinctio dicta facere dolorum explicabo possimus voluptatem?',
             price
         })
+
         await camp.save();
     }
 }
-
-// THIS FILE'S ONLY PURPOSE IS TO FEED THE DB WITH CAMPGROUNDs TITLE AND LOCATION
 
 //seedDB(); //executing seedDB function
 seedDB().then(() => {
